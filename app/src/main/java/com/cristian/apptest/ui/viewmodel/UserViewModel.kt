cde48f271bcc2636d7791d7930c3bddaf1972384
cristian <19374417+CristianCubias@users.noreply.github.com>
2022-07-04 13:32:09 -0600

    Finishing the implementation of obtaining an individual user.

--- app/src/main/java/com/cristian/apptest/ui/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/cristian/apptest/ui/viewmodel/UserViewModel.kt
@@ -2,6 +2,7 @@ package com.cristian.apptest.ui.viewmodel
 
 import androidx.lifecycle.*
 import com.cristian.apptest.domain.models.UserModel
+import com.cristian.apptest.domain.usecases.GetUserUseCase
 import com.cristian.apptest.domain.usecases.GetUsersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -10,12 +11,17 @@ import javax.inject.Inject
 @HiltViewModel
 class UserViewModel @Inject constructor(): ViewModel() {
     val _users by lazy { MutableLiveData<List<UserModel>>() }
+    val _user by lazy { MutableLiveData<UserModel>() }
     val users: LiveData<List<UserModel>> get() = _users
+    val user: LiveData<UserModel> get() = _user
 
     fun onCreate() {
         viewModelScope.launch {
-           val data = GetUsersUseCase().invoke()
+            val data = GetUsersUseCase().invoke()
+            //ID TEST
+            val userData = GetUserUseCase().invoke(5)
             _users.value = data
+            _user.value = userData
         }
     }
 }
